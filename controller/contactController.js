289e4e96c367213670eb7fd4dffa8cff31bb14f1
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-07-19 18:42:32 +0530

    feat: implemented Controller

---
+++ controller/contactController.js
@@ -0,0 +1,36 @@
+// @desc Get all contacts
+// @route GET /api/contacts
+// @access public
+const getContacts = (req, res) => {
+	res.status(200).json({ msg: 'Get all contacts' });
+};
+
+// @desc Create new contact
+// @route POST /api/contacts
+// @access public
+const createContact = (req, res) => {
+	res.status(201).json({ msg: 'Create Contact' });
+};
+
+// @desc Get contact
+// @route GET /api/contacts/:id
+// @access public
+const getContact = (req, res) => {
+	res.status(200).json({ msg: `Get Contact of ${req.params.id}` });
+};
+
+// @desc Update contact
+// @route PUT /api/contacts/:id
+// @access public
+const updateContact = (req, res) => {
+	res.status(200).json({ msg: `Update Contact of ${req.params.id}` });
+};
+
+// @desc Delete contact
+// @route DELETE /api/contacts/:id
+// @access public
+const deleteContact = (req, res) => {
+	res.status(200).json({ msg: `Delete Contact of ${req.params.id}` });
+};
+
+module.exports = { getContacts, createContact, getContact, updateContact, deleteContact };
